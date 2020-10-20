852c2b26f3cdaecad1643b93d4f2217f3b94806a
Willian Schenkel <hischenkel@gmail.com>
2020-10-19 22:34:07 -0300

    feat: added Memoria project

---
+++ 02_Memoria/02_Memoria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  02_Memoria
+//
+//  Created by Willian schenkel on 19/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
